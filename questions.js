e4b65923dd9825cb355ede05f94e2e0f0399e7f2
specie103 <thelaw103@gmail.com>
2022-02-04 13:31:52 +0100

    update

--- questions.js
+++ questions.js
@@ -1,109 +1,107 @@
-
 let questions = [
-    {
-        question: 'WhatsApp concurrent model is implemented using _____ programming language.',
-        answers: {
-            optOne: 'Java',
-            optTwo: 'Node.js',
-            optThree: 'Erlang',
-            optFour: 'C'
-        },
-        correctAnswer: 'Erlang'
+  {
+    question:
+      "WhatsApp concurrent model is implemented using _____ programming language.",
+    answers: {
+      optOne: "Java",
+      optTwo: "Node.js",
+      optThree: "Erlang",
+      optFour: "C",
     },
-    {
-        question: 'Which of the following is not a type of computer code related to Program Execution?',
-        answers: {
-            optOne: 'Source code',
-            optTwo: 'Hex Code',
-            optThree: 'Machine Code',
-            optFour: 'Bytecode'
-        },
-        correctAnswer: 'Hex Code'
+    correctAnswer: "Erlang",
+  },
+  {
+    question:
+      "Which of the following is not a type of computer code related to Program Execution?",
+    answers: {
+      optOne: "Source code",
+      optTwo: "Hex Code",
+      optThree: "Machine Code",
+      optFour: "Bytecode",
     },
-    {
-        question: 'The following are programming languages except?',
-        answers: {
-            optOne: 'Javascript',
-            optTwo: 'python',
-            optThree: 'C++',
-            optFour: 'CSS'
-        },
-        correctAnswer: 'CSS'
+    correctAnswer: "Hex Code",
+  },
+  {
+    question: "The following are programming languages except?",
+    answers: {
+      optOne: "Javascript",
+      optTwo: "python",
+      optThree: "C++",
+      optFour: "CSS",
     },
-    {
-        question: 'One of the following is not a javascript library',
-        answers: {
-            optOne: 'ReactJs',
-            optTwo: 'Angular',
-            optThree: 'Vue',
-            optFour: 'Django'
-        },
-        correctAnswer: 'Django'
+    correctAnswer: "CSS",
+  },
+  {
+    question: "One of the following is not a javascript library",
+    answers: {
+      optOne: "ReactJs",
+      optTwo: "Angular",
+      optThree: "Vue",
+      optFour: "Django",
     },
-    {
-        question: 'Which of the following includes Chromes V8 JavaScript Engine?',
-        answers: {
-            optOne: 'Nodejs',
-            optTwo: 'jQuery',
-            optThree: 'npm',
-            optFour: 'Java'
-           
-        },
-        correctAnswer: 'Nodejs'
+    correctAnswer: "Django",
+  },
+  {
+    question: "Which of the following includes Chromes V8 JavaScript Engine?",
+    answers: {
+      optOne: "Nodejs",
+      optTwo: "jQuery",
+      optThree: "npm",
+      optFour: "Java",
     },
-    {
-        question: 'The binary equivalent of 50 is',
-        answers: {
-            optOne: '1010110',
-            optTwo: '1100110',
-            optThree: '110010',
-            optFour: '110101'
-           
-        },
-        correctAnswer: '110010'
+    correctAnswer: "Nodejs",
+  },
+  {
+    question: "The binary equivalent of 50 is",
+    answers: {
+      optOne: "1010110",
+      optTwo: "1100110",
+      optThree: "110010",
+      optFour: "110101",
     },
-    {
-        question: 'The following are tech opportunities except',
-        answers: {
-            optOne: 'Software Engineer',
-            optTwo: 'Full stack Engineer',
-            optThree: 'Backend Engineer',
-            optFour: 'Content Creator',
-           
-        },
-        correctAnswer: 'Content Creator'
+    correctAnswer: "110010",
+  },
+  {
+    question: "The following are tech opportunities except",
+    answers: {
+      optOne: "Software Engineer",
+      optTwo: "Full stack Engineer",
+      optThree: "Backend Engineer",
+      optFour: "Content Creator",
     },
-    {
-        question: 'What does JavaScript return to you if you try to access a object property that does not exist?',
-        answers: {
-            optOne: 'null',
-            optTwo: 'undefined',
-            optThree: '0',
-            optFour: 'Object not found'
-           
-        },
-        correctAnswer: 'undefined'
+    correctAnswer: "Content Creator",
+  },
+  {
+    question:
+      "What does JavaScript return to you if you try to access a object property that does not exist?",
+    answers: {
+      optOne: "null",
+      optTwo: "undefined",
+      optThree: "0",
+      optFour: "Object not found",
     },
-    {
-        question: 'Which type of language is translated entirely to machine language before use?',
-        answers: {
-            optOne: 'Assembly',
-            optTwo: 'Interpreted',
-            optThree: 'Machine',
-            optFour: 'Compiled'
-           
-        },
-        correctAnswer: 'Compiled'
+    correctAnswer: "undefined",
+  },
+  {
+    question:
+      "Which type of language is translated entirely to machine language before use?",
+    answers: {
+      optOne: "Assembly",
+      optTwo: "Interpreted",
+      optThree: "Machine",
+      optFour: "Compiled",
     },
-    {
-        question: 'Which type of error occurs when a programmer misspells a word or instruction?',
-        answers: {
-            optOne: 'Runtime',
-            optTwo: 'Syntax',
-            optThree: 'System',
-            optFour: 'Semantic'
-           
-        },
-        correctAnswer: 'Syntax'
-    }
+    correctAnswer: "Compiled",
+  },
+  {
+    question:
+      "Which type of error occurs when a programmer misspells a word or instruction?",
+    answers: {
+      optOne: "Runtime",
+      optTwo: "Syntax",
+      optThree: "System",
+      optFour: "Semantic",
+    },
+    correctAnswer: "Syntax",
+  },
 ];
